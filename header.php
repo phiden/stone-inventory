da550f82ec3836bdd3e2cc6fec68a2bc7ef7ce26
Sophia Dengo <sdengo@gmail.com>
2014-08-02 18:45:59 -0700

    cleared a whole boatload of mySQL header errors. Headers are touchy things

--- header.php
+++ header.php
@@ -1,24 +1,3 @@
 <?php
 require_once('authenticate.php');
 ?>
-
-<html>
-
-<head>
-
-<title>The Stone Inventory</title>
-
-<link href="css/screen.css" media="screen" rel="stylesheet" type="text/css" />
-
-<script src="//ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
-<script src="js/vendor/jquery.ui.widget.js"></script>
-<script src="js/jquery.iframe-transport.js"></script>
-<script src="js/jquery.fileupload.js"></script>
-
-<script src="js/stone-inventory.js"></script>
-
-</head>
-
-<body>
-
-<div id="global-wrapper">
--- index.php
+++ index.php
@@ -1,10 +1,3 @@
 <? include "header.php"; ?>
-
-Welcome to your own personal database of stones!!
-
 <? include "login.php"; ?>
-
-<a href="upload-stone.php">Upload a new stone</a>
-<a href="view-collection.php">View your collection.</a>
-
-<?include "footer.php"; ?>
+<? include "footer.php"; ?>
