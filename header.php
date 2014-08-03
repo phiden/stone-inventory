1bf863ea8682435661c48bf03e5798df0e1582ee
Sophia Dengo <sdengo@gmail.com>
2014-08-02 19:02:27 -0700

    replacing content in header.php

---
+++ header.php
@@ -0,0 +1,15 @@
+<html>
+	<header>
+	
+		<title>The Stone Inventory</title>
+		<link href="css/screen.css" media="screen" rel="stylesheet" type="text/css" />
+
+		<script src="//ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
+		<script src="js/vendor/jquery.ui.widget.js"></script>
+		<script src="js/jquery.iframe-transport.js"></script>
+		<script src="js/jquery.fileupload.js"></script>
+
+		<script src="js/stone-inventory.js"></script>
+	</header>
+	
+	<body>
